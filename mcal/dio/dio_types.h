166a648ef802e436e24bafece718495f5ed1702f
Abdelmonem Mostafa <48067992+abdelmonem17@users.noreply.github.com>
2020-03-16 13:14:40 +0200

    Update dio_types.h

--- mcal/dio/dio_types.h
+++ mcal/dio/dio_types.h
@@ -1,9 +1,4 @@
-/*
- * dio_types.h
- *
- *  Created on: Jul 8, 2019
- *      Author: Muhammad.Elzeiny
- */
+
 
 #ifndef E15_ARM_REPO_MCAL_DIO_DIO_TYPES_H_
 #define E15_ARM_REPO_MCAL_DIO_DIO_TYPES_H_
